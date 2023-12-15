279488067650a0c39bfdbf2b23914d1b6b6a608e
himanshkukreja <himansharora2001@gmail.com>
2023-12-15 06:34:59 +0530

    Added nginx script

---
+++ nginx/start-nginx.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Fetch the internal IP
+INTERNAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+echo "Fetched Internal IP: $INTERNAL_IP"
+
+
+# Replace the placeholder in the configuration with the actual internal IP
+
+sed "s/\${INTERNAL_IP}/$INTERNAL_IP/g" /etc/nginx/conf.d/cams-biometric.conf.template > /etc/nginx/conf.d/default.conf
+
+# Start nginx
+nginx -g 'daemon off;'
