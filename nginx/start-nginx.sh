e8b8a7b7b6448ef95dcd074d6ae81a4ce87dba6a
himanshkukreja <himansharora2001@gmail.com>
2023-12-15 06:49:10 +0530

    Added nginx script

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -7,7 +7,7 @@ echo "Fetched Internal IP: $INTERNAL_IP"
 
 # Replace the placeholder in the configuration with the actual internal IP
 
-sed "s/\${INTERNAL_IP}/$INTERNAL_IP/g" /etc/nginx/conf.d/cams-biometric.conf.template > /etc/nginx/conf.d/default.conf
+sed "s/\${INTERNAL_IP}/$INTERNAL_IP/g" /etc/nginx/conf.d/job_queue_service.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 nginx -g 'daemon off;'
